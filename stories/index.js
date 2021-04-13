23beeb478ced4d9f5e311837fbf873a4191abc80
Joel Hart <joel.d.hart1@gmail.com>
2021-04-13 16:43:21 -0700

    set up component folder for appts and set up component structure

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import InterviewerList from "components/InterviewerList";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import Button from "components/Button";
+import Appointment from "components/Appointment/index"
 
 storiesOf("Button", module)
   .addParameters({
@@ -126,3 +127,10 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
